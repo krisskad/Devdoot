ed8e5d0caff3680cd6f19257e0db49032e3ff4b1
krisskad <krishna.g.kadam98500@gmail.com>
2021-06-11 16:30:57 +0530

    default

--- MAIN/settings.py
+++ MAIN/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'MAIN.wsgi.application'
 
 
 DATABASES = {
-    'local': {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
             'init_command': 'SET default_storage_engine=INNODB',
@@ -116,7 +116,7 @@ DATABASES = {
         'timeout':20,
         },
 
-    'default': {
+    'local': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
             'init_command': 'SET default_storage_engine=INNODB',
